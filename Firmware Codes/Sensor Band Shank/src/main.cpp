23e1cd405e279945685031c6e1a95dcda2b1eee3
Abdullah Tahir <47785428+engraxis@users.noreply.github.com>
2025-10-21 11:18:33 +0200

    Update main.cpp

--- Firmware Codes/Sensor Band Shank/src/main.cpp
+++ Firmware Codes/Sensor Band Shank/src/main.cpp
@@ -1,4 +1,5 @@
-//#define DEBUG
+// Copyright (c) 2025 Abdullah Tahir
+// Licensed under the MIT License. See LICENSE file in the project root for details.
 
 #include <Arduino.h>
 #include <Wire.h>
